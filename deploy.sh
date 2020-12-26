45a211f0fe229010bae93f7c17cb23ceb8997232
kiranhiremath13 <61296843+kiranhiremath13@users.noreply.github.com>
2020-12-26 10:31:20 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ cd maven
 git clone https://github.com/kiranhiremath13/java-tomcat-maven-example.git
 cd java-tomcat-maven-example
 mvn package
-cd /home/kiran_hiremath13/software/apache-tomcat-9.0.40/bin
-./shutdown.sh
-cd /home/kiran_hiremath13/software/apache-tomcat-9.0.40/webapps
+#cd /var/lib/jenkins/apache-tomcat-9.0.40/bin
+#./shutdown.sh
+cd /var/lib/jenkins/apache-tomcat-9.0.40/webapps
 rm -rf *
-cp /home/kiran_hiremath13/practice1/practice/ant/antrepo/dist/myweb.war /home/kiran_hiremath13/software/apache-tomcat-9.0.40/webapps
-cp /home/kiran_hiremath13/practice1/practice/maven/java-tomcat-maven-example/target/java-tomcat-maven-example-3.0-SNAPSHOT.war /home/kiran_hiremath13/software/apache-tomcat-9.0.40/webapps
-cd /home/kiran_hiremath13/software/apache-tomcat-9.0.40/bin
-             
+
+cp /var/lib/jenkins/workspace/call-shellscript/practice/maven/java-tomcat-maven-example/target/java-tomcat-maven-example-3.0-SNAPSHOT.war /var/lib/jenkins/apache-tomcat-9.0.40/webapps
+cd /var/lib/jenkins/apache-tomcat-9.0.40/bin
+./startup.sh
